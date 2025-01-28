1b2490753d9a56276789be3852d0d95091b5aa93
Abdelrahman0000 <94646130+Abdelrahman0000@users.noreply.github.com>
2025-01-28 23:25:54 +0200

    Update Portfolio.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -43,7 +43,7 @@ const Portfolio = () => {
     <div className="portfolio" id="portfolio">
       {/* heading */}
       <span style={{color: darkMode?'white': ''}}>Recent Projects</span>
-      <span>Portfolio</span>
+      <span>  Portfolio</span>
 
       {/* slider */}
      
